e893aa01f5645abc182e25bb532fee0f14a0bc73
AlanSoaresDeOliveira <alan.oliveira19@fatec.sp.gov.br>
2020-09-27 13:31:12 -0300

    Add a method to request the API

--- Pokedex/Pokedex/PokemonViewController.swift
+++ Pokedex/Pokedex/PokemonViewController.swift
@@ -11,14 +11,52 @@ import UIKit
 class PokemonViewController: UIViewController {
     @IBOutlet var nameLabel: UILabel!
     @IBOutlet var numberLabel: UILabel!
+    @IBOutlet var type1label: UILabel!
+    @IBOutlet var type2label: UILabel!
     
     var pokemon: Pokemon!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        nameLabel.text = pokemon.name
-        numberLabel.text = String(format: "#%03d", pokemon.number)
+        type1label.text = ""
+        type2label.text = ""
+        
+        
+        let url = URL(string: pokemon.url)
+        guard let u = url else {
+            return
+        }
+        
+        URLSession.shared.dataTask(with: u) { (data, response, error) in
+            guard let data = data else {
+                return
+            }
+            
+            do {
+                let pokemonData = try JSONDecoder().decode(PokemonData.self, from: data)
+                
+                DispatchQueue.main.async {
+                    self.nameLabel.text = self.pokemon.name
+                    self.numberLabel.text = String(format: "#%03d", pokemonData.id)
+                    
+                    for typeEntry in pokemonData.types {
+                        if typeEntry.slot == 1 {
+                            self.type1label.text = typeEntry.type.name
+                        }
+                        else if typeEntry.slot == 2 {
+                            self.type2label.text = typeEntry.type.name
+                        }
+                    }
+                }
+                
+            }
+            catch let error {
+                print("\(error)")
+            }
+        }.resume()
+        
+       
     }
 }
 

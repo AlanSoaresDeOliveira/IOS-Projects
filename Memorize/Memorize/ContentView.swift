acebfeb722cae940e732ee3240fa4fe89fdc7104
AlanSoaresDeOliveira <alan.oliveira19@fatec.sp.gov.br>
2020-09-28 14:22:29 -0300

    Create application

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Alan Soares on 28/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("👻")
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Alan Soares on 28/09/20.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

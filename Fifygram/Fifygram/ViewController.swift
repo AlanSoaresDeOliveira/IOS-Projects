bf9274e4c32ac25a0a625ebcf7efa33ee04d4ed8
AlanSoaresDeOliveira <alan.oliveira19@fatec.sp.gov.br>
2020-09-29 14:26:22 -0300

    Add three new function

--- Fifygram/Fifygram/ViewController.swift
+++ Fifygram/Fifygram/ViewController.swift
@@ -7,13 +7,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    let contex = CIContext()
+    var original: UIImage?
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBAction func choosePhoto() {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let picker = UIImagePickerController()
+            picker.delegate = self
+            picker.sourceType = .photoLibrary
+            navigationController?.present(picker, animated: true, completion: nil)
+        }
     }
-
+    
+    @IBAction func applySepia() {
+        if original == nil {
+            return
+        }
+        
+        let filter = CIFilter(name: "CISepiaTone")
+        filter?.setValue(0.5, forKey: kCIInputIntensityKey)
+        display(filter: filter!)
+        
+    }
+    
+    @IBAction func applyNoip() {
+        if original == nil {
+            return
+        }
+        
+        let filter = CIFilter(name: "CIPhotoEffectNoir")
+        display(filter: filter!)
+        
+    }
+    
+    @IBAction func applyVintage() {
+        if original == nil {
+            return
+        }
+        
+        let filter = CIFilter(name: "CIPhotoEffectProcess")
+        display(filter: filter!)
+    }
+    
+    func display(filter: CIFilter) -> Void {
+        filter.setValue(CIImage(image: original!), forKey: kCIInputImageKey)
+        let output = filter.outputImage
+        imageView.image = UIImage(cgImage: self.contex.createCGImage(output!, from: output!.extent)!)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        navigationController?.dismiss(animated: true, completion: nil)
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = image
+            original = image
+        }
+    }
 
 }
 

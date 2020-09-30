2cac698dccf161ac3aa9ec248c6123437a7b285d
AlanSoaresDeOliveira <alan.oliveira19@fatec.sp.gov.br>
2020-09-30 16:07:19 -0300

    Change the colors of buttons

--- Calculator/Calculator/ContentView.swift
+++ Calculator/Calculator/ContentView.swift
@@ -7,19 +7,64 @@
 
 import SwiftUI
 
+enum CalculatorButton: String {
+    case zero, one, two, three, four, five, six, seven, eight, nine, dot
+    case equals, plus, minus, multiply, divide
+    case ac, plusMinus, percent
+    
+    var title: String {
+        switch self {
+        case .zero: return "0"
+        case .one: return "1"
+        case .two: return "2"
+        case .three: return "3"
+        case .four: return "4"
+        case .five: return "5"
+        case .six: return "6"
+        case .seven: return "7"
+        case .eight: return "8"
+        case .nine: return "9"
+        case .dot: return "."
+        case .plusMinus: return "+/-"
+        case .percent: return "%"
+        case .divide: return "/"
+        case .multiply: return "X"
+        case .minus: return "-"
+        case .plus: return "+"
+        case .equals: return "="
+            
+        default:
+            return "AC"
+        }
+    }
+    
+    var background: Color {
+        switch self {
+        case .zero, .one, .two, .three, .four, .five, .six, .seven, .eight, .nine, .dot:
+            return Color(.darkGray)
+        case .ac, .plusMinus, .percent:
+            return Color(.lightGray)
+        default:
+            return .orange
+        }
+    }
+}
+
 struct ContentView: View {
-    let buttons = [
-        ["7", "8", "9", "X",],
-        ["4", "5", "6", "-",],
-        ["1", "2", "3", "+",],
-        ["0", ".", ".", "=",],
+    let buttons: [[CalculatorButton]] = [
+        [.ac, .plusMinus, .percent, .divide],
+        [.seven, .eight, .nine, .multiply],
+        [.four, .five, .six, .minus],
+        [.one, .two, .three, .plus],
+        [.zero, .dot, .equals],
     ]
     var body: some View {
+        
         ZStack(alignment: .bottom)  {
           
             Color.black.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             
-            VStack(spacing: 10){
+            VStack(spacing: 12){
                 
                 HStack {
                     Spacer()
@@ -27,14 +72,20 @@ struct ContentView: View {
                 }.padding()
                 
                 ForEach(buttons, id: \.self) { row in
-                    HStack {
+                    HStack (spacing: 12){
                         ForEach(row, id: \.self) { button in
-                            Text(button)
-                                .font(.system(size: 32))
-                                .frame(width: buttonWidth(), height: buttonWidth())
-                                .foregroundColor(.white)
-                                .background(Color.yellow)
-                                .cornerRadius(buttonWidth())
+                            
+                            Button(action: {
+                                
+                            }){
+                                Text(button.title)
+                                    .font(.system(size: 32))
+                                    .frame(width: buttonWidth(button: button), height: (UIScreen.main.bounds.width - 5 * 12) / 4)
+                                    .foregroundColor(.white)
+                                    .background(button.background)
+                                    .cornerRadius(buttonWidth(button: button))
+                            }
+                            
                         }
                     }
                 }
@@ -42,7 +93,10 @@ struct ContentView: View {
         }
     }
     
-    func buttonWidth() -> CGFloat {
+    func buttonWidth(button: CalculatorButton) -> CGFloat {
+        if button == .zero {
+            return (UIScreen.main.bounds.width - 4 * 12) / 4 * 2
+        }
         return (UIScreen.main.bounds.width - 5 * 12) / 4
     }
 }

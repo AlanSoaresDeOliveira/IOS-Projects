4b6f9723a9a2da66a50a2b45157c018448ef3117
AlanSoaresDeOliveira <alan.oliveira19@fatec.sp.gov.br>
2020-10-01 16:25:30 -0300

    Create application

---
+++ Bulleye/Bulleye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bulleye
+//
+//  Created by Alan Soares on 01/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

1967f1047607f11686848d194edbc07cb61becdc
AlanSoaresDeOliveira <alan.oliveira19@fatec.sp.gov.br>
2020-10-02 15:07:43 -0300

    Add new function

--- BullsEyeUiKit/BullsEyeUiKit/ViewController.swift
+++ BullsEyeUiKit/BullsEyeUiKit/ViewController.swift
@@ -16,7 +16,18 @@ class ViewController: UIViewController {
     
     @IBAction func showAlert() {
         let alert = UIAlertController(title: "Hello World!", message: "This is my first app!", preferredStyle: .alert)
+        
         let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func knockKnock() {
+        let alert = UIAlertController(title: "Knock Knock!", message: "Who's There?", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Little old lady", style: .default, handler: nil)
         
         alert.addAction(action)
         

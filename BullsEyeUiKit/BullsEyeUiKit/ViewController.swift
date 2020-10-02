bf67d857765680447a1e0e7e6c7aa585605f8eb9
AlanSoaresDeOliveira <alan.oliveira19@fatec.sp.gov.br>
2020-10-02 15:29:04 -0300

    add news buttons and labels

--- BullsEyeUiKit/BullsEyeUiKit/ViewController.swift
+++ BullsEyeUiKit/BullsEyeUiKit/ViewController.swift
@@ -24,15 +24,7 @@ class ViewController: UIViewController {
         present(alert, animated: true, completion: nil)
     }
     
-    @IBAction func knockKnock() {
-        let alert = UIAlertController(title: "Knock Knock!", message: "Who's There?", preferredStyle: .alert)
-        
-        let action = UIAlertAction(title: "Little old lady", style: .default, handler: nil)
-        
-        alert.addAction(action)
-        
-        present(alert, animated: true, completion: nil)
-    }
+    
 
 
 }

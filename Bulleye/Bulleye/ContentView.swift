b470eaaa0ad7a529f3c2a01705e6ccffffb4dccb
AlanSoaresDeOliveira <alan.oliveira19@fatec.sp.gov.br>
2020-10-07 15:51:33 -0300

    Add Spacer() to separete the components

--- Bulleye/Bulleye/ContentView.swift
+++ Bulleye/Bulleye/ContentView.swift
@@ -14,11 +14,20 @@ struct ContentView: View {
         
         VStack {
             // Target row
-            Text("Welcome to my first app!")
-                .fontWeight(.semibold)
-                .foregroundColor(Color.green)
+            Spacer()
+            HStack {
+                Text("Put the bullseye as close as you can to:")
+                Text("100")
+            }
+            Spacer()
             // Slider row
-            
+            HStack {
+                Text("1")
+                Slider(value: .constant(10))
+                
+                Text("100")
+            }
+            Spacer()
             // Button row
             Button(action: {
                 print("Button pressed")
@@ -30,7 +39,23 @@ struct ContentView: View {
                     Alert in
                     return Alert(title: Text("Hello There!"), message: Text("This is my first pop-up"), dismissButton: .default(Text("Awesome!")))
                 }
+            Spacer()
             // Score row
+            HStack {
+                Button(action:  {}) {
+                    Text("Start over")
+                }
+                Spacer()
+                Text("Score:")
+                Text("999999")
+                Spacer()
+                Text("Round:")
+                Text("999")
+                Spacer()
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Text("Info")
+                })
+            }.padding(.bottom, 30)
         }
     }
 }

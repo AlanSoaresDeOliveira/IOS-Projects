69257d92649b562a794c00e705fb3ff0161d9e7b
AlanSoaresDeOliveira <alan.oliveira19@fatec.sp.gov.br>
2020-10-07 12:30:31 -0300

    Add a new button

--- Bulleye/Bulleye/ContentView.swift
+++ Bulleye/Bulleye/ContentView.swift
@@ -9,23 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     @State var alertIsVisible: Bool = false
+    @State var whosThereIsVisible: Bool = false
     
     var body: some View {
+        
         VStack {
             Text("Welcome to my first app!")
                 .fontWeight(.semibold)
                 .foregroundColor(Color.green)
+            
             Button(action: {
                 print("Button pressed")
                 self.alertIsVisible = true
             }) {
                 Text("Hit me!")
             }
-            .alert(isPresented: $alertIsVisible) { () ->
-                Alert in
-                return Alert(title: Text("Hello There!"), message: Text("This is my first pop-up"), dismissButton: .default(Text("Awesome!")))
+                .alert(isPresented: $alertIsVisible) { () ->
+                    Alert in
+                    return Alert(title: Text("Hello There!"), message: Text("This is my first pop-up"), dismissButton: .default(Text("Awesome!")))
+                }
+            
+            Button(action: {
+                self.whosThereIsVisible = true
+            }) {
+                Text("Knock Knock")
             }
+                .alert(isPresented: $whosThereIsVisible) { () ->
+                    Alert in
+                    return Alert(title: Text("Whos There?"), message: Text("It's me, Alan"), dismissButton: .default(Text("Awesome!")))
+                }
+                .frame(height: 50)
+                .padding(.all)
+            
         }
+        
     }
 }
 

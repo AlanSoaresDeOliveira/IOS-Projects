d065883c0d2ae8eb070ee79cea62d79d289ef2ff
AlanSoaresDeOliveira <alan.oliveira19@fatec.sp.gov.br>
2020-10-08 16:48:15 -0300

    Add a new function to calculate the difference between target and sliderValue

--- Bulleye/Bulleye/ContentView.swift
+++ Bulleye/Bulleye/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     @State var alertIsVisible: Bool = false
     @State var sliderValue: Double = 50.0
     @State var target: Int = Int.random(in: 1...100)
@@ -31,15 +32,19 @@ struct ContentView: View {
             Spacer()
             // Button row
             Button(action: {
-                print("Button pressed")
                 self.alertIsVisible = true
             }) {
                 Text("Hit me!")
             }
                 .alert(isPresented: $alertIsVisible) { () ->
                     Alert in
+                    
                     var roundedValue: Int = Int(self.sliderValue.rounded())
-                    return Alert(title: Text("Hello There!"), message: Text("This slider's value is \(roundedValue)"), dismissButton: .default(Text("Awesome!")))
+                    return Alert(title: Text("Hello There!"), message: Text(
+                        "This slider's value is \(roundedValue).\n" +
+                            "You scored \(self.pointsForCurrentRound(value: roundedValue)) points this round"
+                         ), dismissButton: .default(Text("Awesome!")))
+                    
                 }
             Spacer()
             // Score row
@@ -60,6 +65,20 @@ struct ContentView: View {
             }.padding(.bottom, 30)
         }
     }
+    
+    func pointsForCurrentRound(value: Int) -> Int {
+        var difference = self.target - value
+        
+        if difference < 0 {
+            difference = difference * -1
+        }
+        
+        let awardedPoints: Int = 100 - difference
+        
+        return awardedPoints
+    }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {

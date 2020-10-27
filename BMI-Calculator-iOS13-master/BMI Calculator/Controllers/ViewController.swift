265c1669863c9a3ee037538ff871f29ff400d2b6
AlanSoaresDeOliveira <alan.oliveira19@fatec.sp.gov.br>
2020-10-27 09:16:39 -0300

    Link the components

--- BMI-Calculator-iOS13-master/BMI Calculator/Controllers/ViewController.swift
+++ BMI-Calculator-iOS13-master/BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    @IBOutlet weak var heightValueLabel: UILabel!
+    @IBOutlet weak var weightValueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        heightSlider.value = 1.50
+        weightSlider.value = 100
     }
 
-
+    @IBAction func sliderHeightChanged(_ sender: UISlider) {
+        let height = String( format: "%.2f", (sender.value))
+        heightValueLabel.text = "\(height)m"
+    }
+    
+    
+    @IBAction func sliderWeightChanged(_ sender: UISlider) {
+        let weight = String( format: "%.0f", (sender.value))
+        weightValueLabel.text = "\(weight)m"
+    }
+    
 }
 
